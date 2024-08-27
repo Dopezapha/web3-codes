368458e7b8d448b1ec5c0ae25482184f74dfd2e3
Dopezapha <officialnwaneridaniel@gmail.com>
2024-08-27 11:18:51 -0400

    Added UI files

--- Div-Manager/frontend/src/App.js
+++ Div-Manager/frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Connect } from './components/Connect';
+import { ContractInfo } from './components/ContractInfo';
+import { UserActions } from './components/UserActions';
+import { AdminActions } from './components/AdminActions';
+import { useUser } from './contexts/UserContext';
 
 function App() {
+  const { userAddress } = useUser();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>STX Dividend Distribution</h1>
+      <Connect />
+      {userAddress && (
+        <>
+          <ContractInfo />
+          <UserActions />
+          <AdminActions />
+        </>
+      )}
     </div>
   );
 }
